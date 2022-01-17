8cfc9afb52a9d54f9f8a2f4f10749df335a4c424
Bartłomiej Mirek <bwmirek@gmail.com>
2022-01-17 22:26:10 +0100

    init commit

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>33183</title>
+        <link rel="icon" href="/favicon.ico"/>
+      </Head>
+
+      <main>
+        <h1>
+          Hello World
+        </h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
